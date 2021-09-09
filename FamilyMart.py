3fc23a7ef644725daadcfc4c2c8c97acb9aa9e35
ceciChWKao <chwkao91@ceci.com.tw>
2021-09-09 16:43:05 +0800

    [feat]create empty modules

---
+++ FamilyMart.py
@@ -0,0 +1,1 @@
+import approach as ap
---
+++ approach.py
@@ -0,0 +1,7 @@
+import requests as rq
+import selenium as sl
+from bs4 import BeautifulSoup as bs
+from pyquery import PyQuery as pq
+
+# input: url
+# export: data
